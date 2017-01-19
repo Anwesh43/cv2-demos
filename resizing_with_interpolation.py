ac0171a3f784c448e9cdb0ae9a2df7961a15926a
Anwesh43 <anweshthecool0@gmail.com>
2017-01-19 14:09:35 +0530

    Adding return statements in the functions

--- resizing_with_interpolation.py
+++ resizing_with_interpolation.py
@@ -1,12 +1,12 @@
 import cv2
 def resizeWithFScale(im,x,y,interp):
-    cv2.resize(im,None,fx=x,fy=y,interpolation=interp)
+    return cv2.resize(im,None,fx=x,fy=y,interpolation=interp)
 
 def resizeWithSize(im,w,h,interp):
-    cv2.resize(im,(w,h),interpolation=interp)
+    return cv2.resize(im,(w,h),interpolation=interp)
 im = cv2.imread('arsenal-mesut-ozil.jpg')
 
-im1 = resizeWithFScale(im,2,2,cv2.INTER_LINEAR)
+im1 = resizeWithFScale(im,0.5,0.5,cv2.INTER_LINEAR)
 cv2.imshow('half of the image',im1)
 cv2.waitKey(0)
 im2 = resizeWithSize(im,500,500,cv2.INTER_LINEAR)
