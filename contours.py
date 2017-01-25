96bddf3c7f60138b852485af1d192baf0488fec4
Anwesh43 <anweshthecool0@gmail.com>
2017-01-26 00:23:03 +0530

    Displaying largest contour

--- contours.py
+++ contours.py
@@ -10,15 +10,35 @@ def draw_contours(im,contours,color=(0,255,0),n=-1):
     cv2.imshow('contours',im)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+def draw_largest_contour(im,contours,color=(255,0,0),thickness=3):
+    index = 0
+    largestContourIndex = -1
+    maxLen = 0
+    for contour in contours:
+        if(len(contour)>maxLen):
+            maxLen = len(contour)
+            largestContourIndex = index
+        index = index+1
+    cv2.drawContours(im,contours,largestContourIndex,color,thickness)
+    cv2.imshow('largest contour',im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 def draw_contour_from_file(file_name):
     im = cv2.imread(file_name)
     grs = get_grayscale_image(im)
     edged = detect_edge(grs)
     contours = get_contours(edged)
     draw_contours(im,contours)
-    print len(contours)
-def main():
-    draw_contour_from_file('arsenal-mesut-ozil.jpg')
+def draw_largest_contour_from_file(file_name):
+    im = cv2.imread(file_name)
+    grs = get_grayscale_image(im)
+    edged = detect_edge(grs)
+    contours = get_contours(edged)
+    draw_largest_contour(im,contours)
 
+def main(file_name='arsenal-mesut-ozil.jpg'):
+    draw_contour_from_file(file_name)
+    draw_largest_contour_from_file(file_name)
 if __name__ ==  "__main__":
     main()
